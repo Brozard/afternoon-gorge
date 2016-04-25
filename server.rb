e5835504c48dd1c1a66d3cabc1d373b71b8bd101
Brian Nickerson <nickerson.brian@gmail.com>
2016-04-25 18:12:40 -0400

    Updating server.rb

--- server.rb
+++ server.rb
@@ -1,9 +1,22 @@
 require "sinatra"
+require "httparty"
+require "nokogiri"
+require "json"
 
-get "/" do
-  File.read('public/hello.txt')
+class Stock
+  attr_reader :my_span
+  def initialize(symbol)
+    url = "http://finance.yahoo.com/q?s=#{symbol.downcase}"
+    response = HTTParty.get url
+    dom = Nokogiri::HTML(response.body)
+    @my_span = dom.xpath("//span[@id='yfs_l84_#{symbol.downcase}']").first.content
+  end
 end
 
-get "/sinatra" do
-  "Hello Sinatra"
+# stock = Stock.new
+
+get "/" do
+  symbol = params['stock']
+  stock = Stock.new(symbol)
+  "The stock price for #{symbol.upcase} is $#{stock.my_span}"
 end
